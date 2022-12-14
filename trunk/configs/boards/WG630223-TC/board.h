82c6b82f85e81c2dbe60f702282f76be61b2fb71
akw2312 <akw2312@gmail.com>
2022-12-14 14:52:30 +0800

    Add support for Arcadyan WG630223-TC

---
+++ trunk/configs/boards/WG630223-TC/board.h
@@ -0,0 +1,21 @@
+/* WG630223-TC */
+
+#define BOARD_PID		"WG630223-TC"
+#define BOARD_NAME		"WG630223-TC"
+#define BOARD_DESC		"Arcadyan WG630223-TC Wireless Router"
+#define BOARD_VENDOR_NAME	"Arcadyan"
+#define BOARD_VENDOR_URL	"https://www.arcadyan.com/"
+#define BOARD_MODEL_URL		"https://www.arcadyan.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_HAS_5G_11AX	1
+#define BOARD_HAS_2G_11AX	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
